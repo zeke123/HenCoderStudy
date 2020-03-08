f366dc7af2a7ebb88cda8585feea9081dee6edb6
zhoujian <zekezhoujian@163.com>
2020-03-08 20:33:56 +0800

    Java IO 学习

---
+++ iodemo/src/main/java/com/zhoujian/iodemo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.zhoujian.iodemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // git update-index --assume-unchanged file/path  忽略文件
+
+
+    }
+}
