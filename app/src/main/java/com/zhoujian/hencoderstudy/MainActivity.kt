be0d4e5d3776fc395306090d2b1fa3ddb700d8d8
zhoujian <zekezhoujian@163.com>
2020-03-08 22:43:39 +0800

    提交代码

--- app/src/main/java/com/zhoujian/hencoderstudy/MainActivity.kt
+++ app/src/main/java/com/zhoujian/hencoderstudy/MainActivity.kt
@@ -8,5 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //git rm --cached filepath  移除某个文件不再版本控制范围内
     }
 }
